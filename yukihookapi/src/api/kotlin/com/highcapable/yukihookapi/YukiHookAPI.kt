58a6b7206c1ff858f2d7cec52b568295f626f228
Fankesyooni <qzmmcn@163.com>
2022-04-04 02:43:08 +0800

    Update version to 1.0.7

--- yukihookapi/src/api/kotlin/com/highcapable/yukihookapi/YukiHookAPI.kt
+++ yukihookapi/src/api/kotlin/com/highcapable/yukihookapi/YukiHookAPI.kt
@@ -69,10 +69,10 @@ object YukiHookAPI {
     internal var isLoadedFromBaseContext = false
 
     /** 获取当前 [YukiHookAPI] 的版本 */
-    const val API_VERSION_NAME = "1.0.69"
+    const val API_VERSION_NAME = "1.0.7"
 
     /** 获取当前 [YukiHookAPI] 的版本号 */
-    const val API_VERSION_CODE = 14
+    const val API_VERSION_CODE = 15
 
     /**
      * 获取当前 Hook 框架的名称
