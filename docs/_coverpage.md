ae94fdc767b2c64b15efa3ea2b963d3497017841
Fankesyooni <qzmmcn@163.com>
2022-05-04 10:04:59 +0800

    Update version to 1.0.83

--- docs/_coverpage.md
+++ docs/_coverpage.md
@@ -12,7 +12,7 @@
 
 - 方便移植 快速上手
 
-<font size=3 style="opacity: 0.6">`更新时间 2022-05-04 09:23`</font>
+<font size=3 style="opacity: 0.6">`更新时间 2022-05-04 09:57`</font>
 
 [GitHub](https://github.com/fankes/YukiHookAPI)
 [Get Started](#介绍)
--- yukihookapi/src/api/kotlin/com/highcapable/yukihookapi/YukiHookAPI.kt
+++ yukihookapi/src/api/kotlin/com/highcapable/yukihookapi/YukiHookAPI.kt
@@ -69,10 +69,10 @@ object YukiHookAPI {
     internal var isLoadedFromBaseContext = false
 
     /** 获取当前 [YukiHookAPI] 的版本 */
-    const val API_VERSION_NAME = "1.0.82"
+    const val API_VERSION_NAME = "1.0.83"
 
     /** 获取当前 [YukiHookAPI] 的版本号 */
-    const val API_VERSION_CODE = 24
+    const val API_VERSION_CODE = 25
 
     /**
      * 获取当前 Hook 框架的名称
