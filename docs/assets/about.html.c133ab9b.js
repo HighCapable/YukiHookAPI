d2e8ac78147d20fa460d7f51ef99fc5b0dd850bb
fankesyooni <qzmmcn@163.com>
2022-09-30 03:32:06 +0800

    Automatically compile documentation (1.1.2)

---
+++ docs/assets/about.html.c133ab9b.js
@@ -0,0 +1,22 @@
+import{_ as o,r as t,o as l,c,b as s,d as e,e as a,a as p}from"./app.78a03919.js";const i={},r=s("h1",{id:"about-this-document",tabindex:"-1"},[s("a",{class:"header-anchor",href:"#about-this-document","aria-hidden":"true"},"#"),a(" About this Document")],-1),d=a("This document is powered by "),h={href:"https://v2.vuepress.vuejs.org/en",target:"_blank",rel:"noopener noreferrer"},b=a("VuePress"),T=a("."),u=s("h2",{id:"license",tabindex:"-1"},[s("a",{class:"header-anchor",href:"#license","aria-hidden":"true"},"#"),a(" License")],-1),I={href:"https://github.com/fankes/YukiHookAPI/blob/master/LICENSE",target:"_blank",rel:"noopener noreferrer"},E=a("The MIT License (MIT)"),O=p(`<div class="language-text ext-text"><pre class="shiki" style="background-color:#22272e;"><code><span class="line"><span style="color:#adbac7;">MIT License</span></span>
+<span class="line"><span style="color:#adbac7;"></span></span>
+<span class="line"><span style="color:#adbac7;">Copyright (C) 2019-2022 HighCapable</span></span>
+<span class="line"><span style="color:#adbac7;"></span></span>
+<span class="line"><span style="color:#adbac7;">Permission is hereby granted, free of charge, to any person obtaining a copy</span></span>
+<span class="line"><span style="color:#adbac7;">of this software and associated documentation files (the &quot;Software&quot;), to deal</span></span>
+<span class="line"><span style="color:#adbac7;">in the Software without restriction, including without limitation the rights</span></span>
+<span class="line"><span style="color:#adbac7;">to use, copy, modify, merge, publish, distribute, sublicense, and/or sell</span></span>
+<span class="line"><span style="color:#adbac7;">copies of the Software, and to permit persons to whom the Software is</span></span>
+<span class="line"><span style="color:#adbac7;">furnished to do so, subject to the following conditions:</span></span>
+<span class="line"><span style="color:#adbac7;"></span></span>
+<span class="line"><span style="color:#adbac7;">The above copyright notice and this permission notice shall be included in all</span></span>
+<span class="line"><span style="color:#adbac7;">copies or substantial portions of the Software.</span></span>
+<span class="line"><span style="color:#adbac7;"></span></span>
+<span class="line"><span style="color:#adbac7;">THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR</span></span>
+<span class="line"><span style="color:#adbac7;">IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,</span></span>
+<span class="line"><span style="color:#adbac7;">FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE</span></span>
+<span class="line"><span style="color:#adbac7;">AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER</span></span>
+<span class="line"><span style="color:#adbac7;">LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,</span></span>
+<span class="line"><span style="color:#adbac7;">OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE</span></span>
+<span class="line"><span style="color:#adbac7;">SOFTWARE.</span></span>
+<span class="line"><span style="color:#adbac7;"></span></span></code></pre></div><p>Copyright \xA9 2019-2022 HighCapable</p>`,2);function N(R,_){const n=t("ExternalLinkIcon");return l(),c("div",null,[r,s("blockquote",null,[s("p",null,[d,s("a",h,[b,e(n)]),T])]),u,s("p",null,[s("a",I,[E,e(n)])]),O])}const A=o(i,[["render",N],["__file","about.html.vue"]]);export{A as default};
